78c1ffc4b12b17496e8153df7490cb18e4b074aa
Leixhik <eduardo.leijapl@uanl.edu.mx>
2026-02-18 18:06:20 -0600

    Primer Commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from "vue";
+import ApplauseCount from './components/ApplauseCount.vue';
+
+const totalAplausos = ref(0)
+</script>
+
+<template>
+  <h1>Total de Aplausos: {{ totalAplausos }}</h1>
+  <ApplauseCount
+    :puntos="totalAplausos"
+    @aumentar="totalAplausos++" />
+</template>
---
+++ src/components/ApplauseCount.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { computed } from 'vue'
+
+  // Declarar que recibire 'puntos'
+  const props = defineProps({
+    puntos: {
+      type: Number,
+      required: true,
+      // default: 0 -- Opcional en este caso, pues es redundante.
+    }
+  })
+  // Declarar que voy a avisar con 'aumentar'
+  const emit = defineEmits(['aumentar'])
+
+  function manejarClick(){
+    // Aqui lanzare ese aviso.
+    // usare la funcion emit('nombre-del-evento')
+    emit('aumentar')
+  }
+
+
+
+  const claseBoton = computed(() => {
+    if (props.puntos > 10) {
+      return 'gold-button'
+    } else {
+      return 'normal-button'
+    }
+  })
+
+</script>
+
+<template>
+  <button
+    :class="claseBoton"
+    @click="manejarClick"
+  >Aplauso</button>
+  <h1>Llevas {{ props.puntos }} aplausos</h1>
+</template>
+
+<style scoped>
+  .normal-button{
+    background-color: green;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    cursor: pointer;
+  }
+  .gold-button{
+    background-color: gold;
+    border: none;
+    color: black;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    cursor: pointer;
+  }
+
+</style>
